00446542b3e4da4c0f50a1a6ad041160ac37d478
ernixl <ernixl@yahoo.com>
2017-04-06 23:20:34 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Here is a webpage containing php</h1>
+<?php
+$x = 5;
+$y = 10;
+$z = $x + $y;
+echo "
+ <p>Variable x is set as {$x}</p>
+ <p>Variable y is set as {$y}</p>
+ <p>x + y is {$z}</p>
+ ";
+?>
+</body>
+</html>
