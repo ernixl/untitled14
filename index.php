de9c3823e13ecde006e19338a169f62b0b5f855e
ernixl <ernixl@yahoo.com>
2017-04-07 07:14:13 +0100

    jhshs

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ Skillz");
 <form>
     <input type="text" placeholder="First Named" name="firstname">
     <input type="text" placeholder="Last Name" name="lastname">
-    <input type="submit" placeholder="kkkk">
+    <input type="submit" value="Go">
 
 </form>
 <?php
